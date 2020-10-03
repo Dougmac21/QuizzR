d8496bc570f43db7190b49ecb00995c50f2f62cb
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-03 20:01:02 +0100

    linked topic table to topic_id in questions table and quiz table

--- pub_quiz_app/db/pub_quiz.sql
+++ pub_quiz_app/db/pub_quiz.sql
@@ -15,7 +15,7 @@ CREATE TABLE questions (
     alt_ans_2 VARCHAR(255),
     alt_ans_3 VARCHAR(255),
     difficulty VARCHAR(255),
-    topic VARCHAR(255)
+    topic_id SERIAL REFERENCES topics(id)
 );
 
 CREATE TABLE quiz (
@@ -23,5 +23,5 @@ CREATE TABLE quiz (
     date VARCHAR(255),
     number_of_questions INT,
     difficulty VARCHAR(255),
-    topic VARCHAR(255)
+    topic_id SERIAL REFERENCES topics(id)
 );
