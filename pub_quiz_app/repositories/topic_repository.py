a882390629c0d880e55f0f4a15af26e7be9e8e35
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-03 20:17:22 +0100

    Wrote out topic repo

--- pub_quiz_app/repositories/topic_repository.py
+++ pub_quiz_app/repositories/topic_repository.py
@@ -2,13 +2,49 @@ from db.run_sql import run_sql
 from models.topic import Topic
 
 #save
+def save(topic)
+    sql = "INSERT INTO topics (topic) VALUES (%s) RETURNING id"
+    values = [topic.topic]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    zombie.id = id
+
 
 #select
+def select(id):
+    sql = "SELECT * FROM topics WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    topic = Topic(result["topic"])
+    return topic
 
 #select-all
+def select_all():
+    topics = []
+    sql = "SELECT * FROM topics"
+    results = run_sql(sql)
+    for result in results:
+        topic = Topic(result["topic"], result["id"])
+        topics.append(topic)
+    return topics
 
 #delete
+def delete(id):
+    sql = "DELETE FROM topics WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
 
 #delete-all
+def delete_all():
+    sql = "DELETE FROM topics"
+    run_sql(sql)
 
 #update
+def update(topic):
+    sql = "UPDATE topics SET (topic) = (%s) WHERE id = %s"
+    values = [topic.topic, topic.id]
+    run_sql(sql, values)
+
+
+## select quizzes of topic
+# extension
