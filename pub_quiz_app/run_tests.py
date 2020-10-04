039c381f832b7e05905e7bd9b3aed8f68f99def9
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-04 12:42:53 +0100

    added test files

---
+++ pub_quiz_app/run_tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from tests.question_test import QuestionTest
+from tests.quiz_test import QuizTest
+from tests.topic_test import TopicTest
+
+if __name__ == "__main__":
+    unittest.main()
+
--- pub_quiz_app/tests/question_test.py
+++ pub_quiz_app/tests/question_test.py
@@ -4,7 +4,7 @@ from models.question import Question
 from models.quiz import Quiz
 from models.topic import Topic
 
-class QuestionsTest(unittest.TestCase):
+class QuestionTest(unittest.TestCase):
 
     def setUp(self):
         self.question = Question("What is 2 plus 2?", "4", "3", "2", "1", "EASY", "Science")
