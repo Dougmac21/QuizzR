1b3a783bb196e1954f7daba0568956e8f7303c2f
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-04 16:41:52 +0100

    HTML work and test refactoring

--- pub_quiz_app/models/quiz.py
+++ pub_quiz_app/models/quiz.py
@@ -1,8 +1,9 @@
 class Quiz():
-    def __init__(self, date, number_of_questions, difficulty, topic, id=None):
+    def __init__(self, date, number_of_questions, difficulty, topic, questions, id=None):
         self.date = date
         self.number_of_questions = number_of_questions
         self.difficulty = difficulty
         self.topic = topic
+        self.questions = []
         self.id = id
 
--- pub_quiz_app/tests/question_test.py
+++ pub_quiz_app/tests/question_test.py
@@ -7,7 +7,7 @@ from models.topic import Topic
 class QuestionTest(unittest.TestCase):
 
     def setUp(self):
-        self.question = Question("What is 2 plus 2?", "4", "3", "2", "1", "EASY", "Science")
+        self.question = Question("What is 2 plus 2?", "4", "3", "2", "1", "EASY", "Science", False)
 
     def test_question_has_parameters(self):
         self.assertEqual("What is 2 plus 2?", self.question.the_question)
@@ -17,9 +17,10 @@ class QuestionTest(unittest.TestCase):
         self.assertEqual("1", self.question.alt_ans_3)
         self.assertEqual("EASY", self.question.difficulty)    
         self.assertEqual("Science", self.question.topic)
+        self.assertEqual(False, self.question.used)
 
     def test_questions_can_be_added(self):
-        self.question2 = Question("What is 3 plus 3?", "6", "5", "4", "3", "MEDIUM", "Maths")
+        self.question2 = Question("What is 3 plus 3?", "6", "5", "4", "3", "MEDIUM", "Maths", False)
         self.assertEqual("What is 3 plus 3?", self.question2.the_question)
         self.assertEqual("6", self.question2.correct_answer)
         self.assertEqual("5", self.question2.alt_ans_1)
@@ -27,3 +28,4 @@ class QuestionTest(unittest.TestCase):
         self.assertEqual("3", self.question2.alt_ans_3)
         self.assertEqual("MEDIUM", self.question2.difficulty)    
         self.assertEqual("Maths", self.question2.topic)
+        self.assertEqual(False, self.question2.used)
