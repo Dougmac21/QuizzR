383aa069fdde6a6ff84dc701d57c106916a8d607
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-04 19:58:14 +0100

    adding distinct difficulties table to database for id linkage for quizzes and questions

--- pub_quiz_app/db/pub_quiz.sql
+++ pub_quiz_app/db/pub_quiz.sql
@@ -1,10 +1,16 @@
 DROP TABLE IF EXISTS topics;
+DROP TABLE IF EXISTS difficulties;
 DROP TABLE IF EXISTS quizzes;
 DROP TABLE IF EXISTS questions;
 
 CREATE TABLE topics (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
+);
+
+CREATE TABLE difficulties (
+    id SERIAL PRIMARY KEY,
+    difficulty VARCHAR(10)
 );
 
 CREATE TABLE questions (
